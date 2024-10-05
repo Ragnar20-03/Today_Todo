c3c699537ae29a62b54a1d2a7e5aa62f6d97ad25
amit-sali <amitsaliap@gmail.com>
2024-10-05 12:00:43 +0530

    changes by amit

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -3,7 +3,7 @@ export default function Nav() {
     <nav className="bg-black text-white p-4 m-3 rounded-md font-serif">
       <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center">
         <div className="mb-4 sm:mb-0">
-          <h1 className="text-xl font-bold">Today_Todo</h1>
+          <h1 className="text-xl font-bold">roshan shubham amit</h1>
         </div>
         <div className="flex space-x-4">
           <a
