ac9c85c7e2ab8174e351ff2b45370ffc773ba8c2
Ragnar20-03 <roshanpp20@gmail.com>
2024-10-05 13:56:26 +0530

    Temp2

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-<<<<<<< HEAD
+
 const { connect, Todo } = require("./Schema");
 const app = express();
 const Port = 3000;
@@ -7,35 +7,30 @@ const Port = 3000;
 app.use(express.json());
 
 app.post("/addTodo", async (req, res) => {
-     const todo = req.body.todo;
-     Todo.create(
-          todo
-     ).then((res1) => {
-          return res.status(200).json({
-               msg: " todo added successfully..."
-          })
-     })
-          .catch((err1) => {
-               console.log("err", err1)
-               return res.status(404).json({
-                    msg: "Error occurred"
-               })
-          })
-})
+  const todo = req.body.todo;
+  Todo.create(todo)
+    .then((res1) => {
+      return res.status(200).json({
+        msg: " todo added successfully...",
+      });
+    })
+    .catch((err1) => {
+      console.log("err", err1);
+      return res.status(404).json({
+        msg: "Error occurred",
+      });
+    });
+});
 
 app.get("/getTodos", async (req, res) => {
-     Todo.find({}).then((res1) => {
-          return res.status(200).json({
-               msg: "Found todo..."
-          })
-     })
-})
+  Todo.find({}).then((res1) => {
+    return res.status(200).json({
+      msg: "Found todo...",
+    });
+  });
+});
 
 app.listen(Port, function () {
-     console.log("Server Started...");
-     connect();
-})
-=======
-
-const app = express();
->>>>>>> 36ba35d ( Temp)
+  console.log("Server Started...");
+  connect();
+});
